b0e8209f175fd7047ad16d27d2d8930661aba5a8
Unknown <DTorricelli045@west-mec.org>
2018-05-14 15:06:14 -0700

    began creating forms

--- application.js
+++ application.js
@@ -39,4 +39,17 @@ for (var i = 0; i < bttn.length; i++) {
   });
 }
 
+
+// Add google maps
+function myMap() {
+  var mapOptions = {
+      center: new google.maps.LatLng(51.5, -0.12),
+      zoom: 10,
+      mapTypeId: google.maps.MapTypeId.HYBRID
+  }
+  var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+  }
+
+
+// Run on load
 window.addEventListener("load", openNav())
