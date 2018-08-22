143868745dc1337d65006953a15d1f470cdc67ad
Unknown <AThompson027@west-mec.org>
2018-08-22 15:16:05 -0700

    Validation for Donate and Question
    Added validation for donate and question input elements, but have bugs for select elements and textarea element. Also added an error message to both forms.

--- valid.js
+++ valid.js
@@ -1,5 +1,159 @@
 // Javascript to validate forms
 
 //form 1 donApply
+
+"use strict";
+
+ var formValidity = true;
+
+function removeSelectDefaults() {
+    var emptyBoxes = document.querySelectorAll("#donApply select");
+    for (var i = 0; i < emptyBoxes.length; i++) {
+        emptyBoxes[i].selectedIndex = -1;
+    }
+}
+
+ // function to validate required fields
+ function validateDonate() {
+     // support variables for validation
+     var inputElements = document.querySelectorAll("#donApply input");
+     var donErr = document.getElementById("donateError");
+     var fieldsetValidity = true;
+     var elementCount = inputElements.length;
+     var currentElement;
+
+     // try catch to handle a loop through the fields
+     try {
+         // loop through input fields looking for blanks
+         for (var i = 0; i < elementCount; i++) {
+             currentElement = inputElements[i];
+             //blanks
+             if (currentElement.value === "") {
+                 currentElement.style.background = "rgb(255,233,233)";
+                 fieldsetValidity = false;
+             } else {
+                 currentElement.style.background = "white";
+             }
+         }
+
+         if (fieldsetValidity === false) {
+             throw "Please complete all donation info."
+         } else {
+             donErr.innerHTML = "none";
+             donErr.style.display = "";
+         }
+         
+          // validate select list field
+        currentElement = document.querySelectorAll("#donApply select");
+        if (currentElement.selectedIndex === -1) {
+            currentElement.style.border = "1px solid red";
+            fieldsetValidity = false;
+        } else {
+            currentElement.style.border = "";
+        }
+         
+     } catch (msg) {
+         donErr.style.display = "block";
+        donErr.innerHTML = msg;
+        formValidity = false;
+     }
+ }
+
+// function to validate required fields
+ function validateQuestion() {
+     // support variables for validation
+     var inputElements = document.querySelectorAll("#questApply input");
+     var queErr = document.getElementById("questError");
+     var fieldsetValidity = true;
+     var elementCount = inputElements.length;
+     var textBox = document.getElementsByTagName("textarea");
+     var currentElement;
+
+     // try catch to handle a loop through the fields
+     try {
+         // loop through input fields looking for blanks
+         for (var i = 0; i < elementCount; i++) {
+             currentElement = inputElements[i];
+             //blanks
+             if (currentElement.value === "") {
+                 currentElement.style.background = "rgb(255,233,233)";
+                 fieldsetValidity = false;
+             } else {
+                 currentElement.style.background = "white";
+             }
+         }
+
+         if (fieldsetValidity === false) {
+             throw "Please complete all question info."
+         } else {
+             queErr.innerHTML = "none";
+             queErr.style.display = "";
+         }
+         if (textBox.value === "") {
+             currentElement.style.background = "rgb(255,233,233)";
+                 fieldsetValidity = false;
+         } else {
+              currentElement.style.background = "white";
+         }
+     } catch (msg) {
+         queErr.style.display = "block";
+        queErr.innerHTML = msg;
+        formValidity = false;
+     }
+ }
+
+
+
+ // function to validate form on submit event
+ function validateForm(evt) {
+     if (evt.preventDefault) {
+         evt.preventDefault();
+     } else {
+         evt.returnValue = false;
+     }
+     formValidity = true;
+
+     validateDonate();
+     validateQuestion()
+
+     // results of validation tests
+     if (formValidity === true) {
+         document.getElementById("donateError").innerHTML = "";
+         document.getElementById("donateError").style.display = "none";
+         document.getElementsByTagName("form")[0].submit();
+     } else {
+         document.getElementById("donateError").innerHTML = "Please fix the indicated problems.";
+         document.getElementById("donateError").style.display = "block";
+         scroll(0, 0);
+     }
+ }
+
+ // function to create our event listeners
+ function createEventListeners() {
+     // event listener for submit
+     // event handeler validateForm() 
+     if (window.addEventListener) {
+         window.addEventListener("submit", validateForm, false);
+     } else if (window.attachEvent) {
+         window.attachEvent("onsubmit", validateForm);
+     }
+ }
+
+ // add a load event listener
+ //event handler will be createEventListeners()
+
+ if (window.addEventListener) {
+     window.addEventListener("load", createEventListeners, false);
+ } else if (window.attachEvent) {
+     window.attachEvent("onload", createEventListeners);
+ }
+
+
+
+
+
 //form 2 jobApply
+
+
+
 //form 3 questApply
